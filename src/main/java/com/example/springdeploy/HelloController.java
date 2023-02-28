18370a1534c25c235f3d0d4dc03e23fbccc512e7
Pedro José Muñoz Reyes <pedrojmure@gmail.com>
2023-02-28 11:32:01 +0100

    Initial commit b1

--- src/main/java/com/example/springdeploy/HelloController.java
+++ src/main/java/com/example/springdeploy/HelloController.java
@@ -31,7 +31,7 @@ public class HelloController {
                     <!-- Bootstrap CSS -->
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
                                 
-                    <title>Hello, world!</title>
+                    <title>Hola MUNDO DESDE BOOTSTRAP!!!</title>
                   </head>
                   <body>
                     <h1>Hola mundo desde Srping Boot!</h1>
@@ -64,7 +64,7 @@ public class HelloController {
                     <!-- Bootstrap CSS -->
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
                                 
-                    <title>Hello, world!</title>
+                    <title>Hola MUNDO!!!</title>
                   </head>
                   <body>
                     <h1>Hola mundo desde Srping Boot!</h1>
