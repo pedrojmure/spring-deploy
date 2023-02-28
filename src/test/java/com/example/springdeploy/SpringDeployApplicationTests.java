e2af11b833dab01049da4075083ead5bf291f177
Pedro José Muñoz Reyes <pedrojmure@gmail.com>
2023-02-28 11:53:57 +0100

    Initial commit b1

--- src/test/java/com/example/springdeploy/SpringDeployApplicationTests.java
+++ src/test/java/com/example/springdeploy/SpringDeployApplicationTests.java
@@ -8,9 +8,6 @@ class SpringDeployApplicationTests {
 
 	@Test
 	void contextLoads() {
-		System.getenv().forEach (
-				(key,value) -> System.out.println("key "+ key+ " value "+value)
-		);
 	}
 
 }
